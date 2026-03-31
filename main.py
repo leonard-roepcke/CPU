dc3152263ee59f17ca0d2d5f9e327a153ce6be27
leonard-roepcke <leonard.roepcke.2@gmail.com>
2026-03-31 13:15:09 +0200

    not funktioning ram adrresing

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ print("CPU started")
 from utils import *
 
 ram = Ram_8byte()
-ram.write([0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,1])
-a = Safe_8bit([0,0,0,0,0,0,1,1])
-print(ram.read(a.read()))
+ram.write([0,0,0,0,0,0,0,1],[0,0,0,0,0,0,1,1])
+a = Safe_8bit([0,0,0,0,0,0,0,0])
+
+for i in range(8):
+    print(a.read() , "   " , ram.read(a.read()))
+    a.write(add_8bit(a.read(),[0,0,0,0,0,0,0,1])[0])
+
 """
 a = Safe_8bit()
 inp = Safe_8bit()
